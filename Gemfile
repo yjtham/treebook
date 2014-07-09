99528b177af47729c51c08b6edfbc8ea742df40c
Yih Jiun <yjtham@hotmail.com>
2014-07-09 23:29:58 +0800

    Updated frontend for the new authentication system
    Added simple_form
    Updated several forms to bootstrap/ simple_form
    Added login/logout/reigster/profile links to our nav
    Added a collection select to our status form

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '4.1.1'
 gem 'sqlite3'
 
 gem 'devise'
+
+gem 'simple_form'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+class ApplicationController < ActionController::Base 
+	before_action :configure_permitted_parameters, if: :devise_controller? 
+	# Prevent CSRF attacks by raising an exception. 
+	# For APIs, you may want to use :null_session instead. 
+	protect_from_forgery with: :exception 
+	
+	def configure_permitted_parameters 
+		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :profile_name, :email, :password, :password_confirmation) } 
+	end 
+
+	
 end
+
